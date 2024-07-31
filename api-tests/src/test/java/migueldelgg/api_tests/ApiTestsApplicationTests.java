2c11faebaba8b3d13a498b7498b3cc2b5528908c
Miguel <delgdevoleper@gmail.com>
2024-07-31 18:11:07 -0300

    Initial commit

---
+++ api-tests/src/test/java/migueldelgg/api_tests/ApiTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package migueldelgg.api_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
